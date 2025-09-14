8333f556c3d194c05341bd2424ad67252e58a3b9
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-14 00:11:44 -0300

    Create Produto.java

---
+++ supermercado/Produto.java
@@ -0,0 +1,35 @@
+public class Produto {
+	public Descricao descricao;
+	public int tipo;
+	public double preco;
+	public int quantidadeEstoque;
+
+	public Produto(int t, int v, int q) {
+		switch(t) {
+		case 1:
+			descricao = Descricao.ARROZ;
+			break;
+		case 2:
+			descricao = Descricao.FEIJAO;
+			break;
+		case 3:
+			descricao = Descricao.FARINHA;
+			break;
+		case 4:
+			descricao = Descricao.LEITE;
+			break;
+		}
+
+		tipo = t;
+		preco = v;
+		quantidadeEstoque = q;
+	}
+
+	public int intDescricao() {
+		return tipo;
+	}
+
+	public String mostraProduto() {
+		return "\n Produto -> Tipo: " + descricao + "\n\t    Quantidade: " + quantidadeEstoque + "\n\t    Preco: " + preco;
+	}
+}
