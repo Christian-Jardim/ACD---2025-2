4966633107a82c18411adfa3983192e4c27c3531
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-16 09:07:15 -0300

    Update Main.java

--- circuito de resistores/src/Main.java
+++ circuito de resistores/src/Main.java
@@ -7,40 +7,45 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 
 		do {
-			System.out.print("\n 1 - Adicionar circuito\n 0 - Sair\n Escolha uma opcao: ");
+			System.out.print("\n 1 - Adicionar circuito\n 0 - Sair\n Escolha uma opcao: "); // Opcao para sair ou adicionar circuito
 			op1 = scanner.nextInt();
 
-			if(op1 != 0) {
-				System.out.print("\n Quantos resistores deseja adicionar? ");
+			if(op1 == 1) {
+				System.out.print("\n Quantos resistores deseja adicionar? "); // Recebe a quantidade de resistores
 				int quant = scanner.nextInt();
 
 				ArrayList<Resistor> resistores = new ArrayList<>();
-				for (int i = 1; i <= quant; i++) {
-					System.out.print(" Digite o valor do resistor R" + i + " (em ohms): ");
+				for (int i = 1; i <= quant; i++) { // Loop para preencher o circuito
+					System.out.print(" Digite o valor do resistor R" + i + " (em ohms): "); // Recebe o valor (em ohms) de cada resistor
 					double value = scanner.nextDouble();
 					resistores.add(new Resistor(value));
 				}
 
-				System.out.print("\n O circuito e em Serie (1) ou Paralelo (2)? ");
+				System.out.print("\n O circuito e em Serie (1) ou Paralelo (2)? "); // Usuario decide se sera em serie ou paralelo
 				op2 = scanner.nextInt();
 
 				Circuit circuito;
 
-				if (op2 == 1) {
+				if (op2 == 1) { // calcula a resistencia equivalente para o circuito em serie
 					Serial serie = new Serial();
 					for (Resistor r : resistores) {
 						serie.add(r);
 					}
 					circuito = serie;
-				} else {
+				} 
+				else if(op2 == 2) { // calcula a resistencia equivalente para o circuito em paralelo
 					Parallel paralelo = new Parallel();
 					for (Resistor r : resistores) {
 						paralelo.add(r);
 					}
 					circuito = paralelo;
+				} else {
+					System.out.print("\n Opcao invalida!");
 				}
-
+				// Mostra a resistencia equivalente de acordo com o tipo de circuito escolhido
 				System.out.println("\n Resistencia equivalente: " + circuito.getResistance() + " ohms");
+			} else {
+				System.out.print("\n Opcao invalida!");
 			}
 		} while(op1 != 0);
 	}
